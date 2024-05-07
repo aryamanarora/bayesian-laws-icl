be35f968188811c5d14009e4179d53ef283be0a0
aryamanarora <aryaman.arora2020@gmail.com>
2024-05-06 18:09:07 -0700

    working HMM replication now, SFT exp

--- data.py
+++ data.py
@@ -2,6 +2,8 @@ import numpy as np
 from tqdm import tqdm
 from torch.utils.data import Dataset
 from typing import Union
+from collections import defaultdict
+from copy import deepcopy
 
 
 class XieHMM:
@@ -12,11 +14,15 @@ class XieHMM:
     start_entity_probs: np.ndarray # shape: (num_entities,)
     start_property_probs: np.ndarray # shape: (num_properties,)
 
-    def __init__(self, num_entities: int, num_properties: int):
+    def __init__(
+        self, num_entities: int, num_properties: int, vocab_size: int=100,
+        entity_transition_probs: np.ndarray=None, emission_mapping: np.ndarray=None,
+    ):
         self.num_entities = num_entities
         self.num_properties = num_properties
+        self.num_hidden_states = num_entities * num_properties
 
-        # generate T
+        # generate property probs
         # first need 100 random permutation matrices of size (num_properties, num_properties)
         weights = (np.random.random(100) - 0.5) / 0.1
         weights = np.exp(weights) / np.sum(np.exp(weights))
@@ -31,35 +37,79 @@ class XieHMM:
         T_s = np.exp(T_s) / np.sum(np.exp(T_s))
         self.start_property_probs = T_s
 
-        # generate S
+        # generate entity probs
         # same process
-        weights = (np.random.random(100) - 0.5) / 0.1
-        weights = np.exp(weights) / np.sum(np.exp(weights))
-        S = np.zeros((num_entities, num_entities))
-        for i in range(100):
-            permutation_matrix = np.random.permutation(np.eye(num_entities))
-            S += weights[i] * permutation_matrix
-        self.entity_transition_probs = S * 0.1 + np.eye(num_entities) * 0.9
+        if entity_transition_probs is None:
+            weights = (np.random.random(100) - 0.5) / 0.1
+            weights = np.exp(weights) / np.sum(np.exp(weights))
+            S = np.zeros((num_entities, num_entities))
+            for i in range(100):
+                permutation_matrix = np.random.permutation(np.eye(num_entities))
+                S += weights[i] * permutation_matrix
+            self.entity_transition_probs = S * 0.1 + np.eye(num_entities) * 0.9
+        else:
+            self.entity_transition_probs = entity_transition_probs
 
-        # start
         S_s = (np.random.random(num_entities) - 0.5) / 10
         S_s = np.exp(S_s) / np.sum(np.exp(S_s))
         self.start_entity_probs = S_s
+
+        # combine entity and property transitions/start probs into one matrix
+        transition = np.zeros((num_entities * num_properties, num_entities * num_properties))
+        for i in range(num_entities):
+            for j in range(num_entities):
+                transition[
+                    i * num_properties: (i + 1) * num_properties, j * num_properties: (j + 1) * num_properties
+                ] = self.property_transition_probs * self.entity_transition_probs[i, j]
+        self.state_transition_probs = transition
+
+        start = np.zeros(num_entities * num_properties)
+        for i in range(num_entities):
+            start[i * num_properties: (i + 1) * num_properties] = self.start_property_probs * self.start_entity_probs[i]
+        self.start_state_probs = start
+
+        # emission mapping
+        if emission_mapping is None:
+            # self.emission_mapping = np.arange(1, num_entities * num_properties + 1).reshape((num_entities, num_properties))
+            self.emission_mapping = np.random.randint(low=1, high=vocab_size, size=(num_entities, num_properties))
+            self.emission_mapping[:, 0] = 0 # delimiter
+            self.emission_mapping = self.emission_mapping.flatten()
+        else:
+            self.emission_mapping = emission_mapping
+
+        # also store as probs
+        self.emission_probs = np.zeros((num_entities * num_properties, vocab_size))
+        for i in range(self.num_hidden_states):
+            self.emission_probs[i, self.emission_mapping[i]] = 1
     
-    def sample(self, length: int):
-        entity = np.random.choice(self.num_entities, p=self.start_entity_probs)
-        property = np.random.choice(self.num_properties, p=self.start_property_probs)
-        emissions, entities, properties = [], [], []
+    def sample(self, length: int, return_argmax: bool=False):
+        state = np.random.choice(self.num_hidden_states, p=self.start_state_probs)
+        states, emissions = [], []
+        argmaxes = []
         for _ in range(length):
-            entities.append(entity)
-            properties.append(property)
-            if property == 0: # delimiter state
-                emissions.append(0)
-            else:
-                emissions.append(entity * self.num_properties + property)
-            entity = np.random.choice(self.num_entities, p=self.entity_transition_probs[entity])
-            property = np.random.choice(self.num_properties, p=self.property_transition_probs[property])
-        return emissions, list(zip(entities, properties))
+            states.append(state)
+            emissions.append(self.emission_mapping[state])
+            if return_argmax:
+                argmax_state = np.argmax(self.state_transition_probs[state])
+                argmaxes.append(self.emission_mapping[argmax_state])
+            state = np.random.choice(self.num_hidden_states, p=self.state_transition_probs[state])
+
+        if return_argmax:
+            return emissions, states, argmaxes
+        
+        return emissions, states
+    
+    def score(self, emissions: list[int]):
+        length = len(emissions)
+        scores = np.zeros((length, self.num_hidden_states)) # in log space
+        for i in range(self.num_hidden_states):
+            scores[0, i] = np.log(self.start_state_probs[i]) + np.log(self.emission_probs[i, emissions[0]])
+        for i in range(1, length):
+            for j in range(self.num_hidden_states):
+                temp = [scores[i - 1, k] + np.log(self.state_transition_probs[k, j]) + np.log(self.emission_probs[j, emissions[i]])
+                        for k in range(self.num_hidden_states)]
+                scores[i, j] = np.logaddexp.reduce(temp)
+        return np.logaddexp.reduce(scores[-1])
 
 
 class HMM:
@@ -103,24 +153,30 @@ class MixtureOfHmms:
     hmms: list[Union[HMM, XieHMM]]
     weights: np.ndarray # shape: (num_hmms,)
 
-    def __init__(self, num_hmms: int, num_hidden_states: int, num_emissions: int,
-                 uniform_weights: bool=False, xie: bool=False):
+    def __init__(self, num_hmms: int, num_entities: int, num_properties: int,
+                 num_emissions: int=100, uniform_weights: bool=False):
+                 
         self.num_hmms = num_hmms
-
-        if xie:
-            self.hmms = [XieHMM(num_hidden_states, num_emissions) for _ in range(num_hmms)]
-        else:
-            self.hmms = [HMM(num_hidden_states, num_emissions) for _ in range(num_hmms)]
+        self.hmms = []
+        for i in range(num_hmms):
+            if i == 0:
+                self.hmms.append(XieHMM(num_entities, num_properties, num_emissions))
+            else:
+                self.hmms.append(XieHMM(
+                    num_entities, num_properties, num_emissions,
+                    entity_transition_probs=self.hmms[0].entity_transition_probs,
+                    emission_mapping=self.hmms[0].emission_mapping,
+                ))
         
         if uniform_weights:
             self.weights = np.ones(num_hmms) / num_hmms
         else:
             self.weights = np.random.dirichlet(np.ones(num_hmms), size=1)[0]
     
-    def sample(self, length: int, hmm=None):
+    def sample(self, length: int, hmm=None, **kwargs):
         if hmm is None:
             hmm = np.random.choice(self.num_hmms, p=self.weights)
-        return self.hmms[hmm].sample(length), hmm
+        return self.hmms[hmm].sample(length, **kwargs), hmm
     
     def score(self, emissions: list[int]):
         scores = [np.log(self.weights[i]) + self.hmms[i].score(emissions) for i in range(self.num_hmms)]
@@ -128,20 +184,24 @@ class MixtureOfHmms:
     
 
 class HMMDataset(Dataset):
-    def __init__(self, hmms: MixtureOfHmms, num_train_examples: int=10000, sample_length: int=1000):
+    def __init__(self, hmms: MixtureOfHmms, num_train_examples: int=10000, sample_length: int=1000, hmm: int=None):
         super(HMMDataset, self).__init__()
         self.hmms = hmms
-        self.length = num_train_examples
         self.emissions = []
         self.states = []
         self.hmm = []
+        self.block_size = 1024
 
         # generate data
-        for _ in tqdm(range(self.length)):
-            (emissions, states), hmm = self.hmms.sample(sample_length)
-            self.emissions.append(emissions)
-            self.states.append(states)
-            self.hmm.append(hmm)
+        for _ in tqdm(range(num_train_examples)):
+            (emissions, states), src_hmm = self.hmms.sample(sample_length, hmm=hmm)
+            for start in range(0, sample_length, self.block_size):
+                end = min(start + self.block_size, sample_length)
+                self.emissions.append(emissions[start:end])
+                self.states.append(states[start:end])
+                self.hmm.append(src_hmm)
+
+        self.length = len(self.hmm)
     
     def __len__(self):
         return self.length
@@ -154,22 +214,50 @@ class HMMDataset(Dataset):
             "hmm": self.hmm[idx],
         }
     
+    def make_subsets(self):
+        datasets = defaultdict(list)
+        for i in range(self.length):
+            item = self[i]
+            datasets[item["hmm"]].append(item)
+        return datasets
 
-class HMMDataset(Dataset):
-    def __init__(self, hmms: MixtureOfHmms, num_train_examples: int=10000, sample_length: int=1000):
-        super(HMMDataset, self).__init__()
+
+class HMMInContextDataset(Dataset):
+    def __init__(self, hmms: MixtureOfHmms, num_train_examples: int=10000, k: int=10, num_in_context_shots: int=64):
+        super(HMMInContextDataset, self).__init__()
         self.hmms = hmms
         self.length = num_train_examples
         self.emissions = []
         self.states = []
+        self.labels = []
         self.hmm = []
 
         # generate data
         for _ in tqdm(range(self.length)):
-            (emissions, states), hmm = self.hmms.sample(sample_length)
-            self.emissions.append(emissions)
-            self.states.append(states)
+            hmm = np.random.choice(self.hmms.num_hmms, p=self.hmms.weights)
+            emissions_doc = []
+            states_doc = []
+            labels_doc = []
+            for i in range(num_in_context_shots):
+
+                # in-context example
+                (emissions, states, labels), _ = self.hmms.sample(k, hmm=hmm, return_argmax=True)
+
+                emissions_doc.extend(emissions)
+                states_doc.extend(states)
+                labels_doc.extend(labels)
+
+                # delimiter
+                if i != num_in_context_shots - 1:
+                    emissions_doc.append(0)
+                    states_doc.append(0)
+                    labels_doc.append(0)
+            
+            # add doc
             self.hmm.append(hmm)
+            self.emissions.append(emissions_doc)
+            self.states.append(states_doc)
+            self.labels.append(labels_doc)
     
     def __len__(self):
         return self.length
@@ -177,7 +265,14 @@ class HMMDataset(Dataset):
     def __getitem__(self, idx):
         return {
             "input_ids": self.emissions[idx],
-            "labels": self.emissions[idx],
+            "labels": self.labels[idx],
             "states": self.states[idx],
             "hmm": self.hmm[idx],
         }
+    
+    def make_subsets(self):
+        datasets = defaultdict(list)
+        for i in range(self.length):
+            item = self[i]
+            datasets[item["hmm"]].append(item)
+        return datasets
--- train.py
+++ train.py
@@ -3,25 +3,35 @@ import torch
 import transformers
 from dataclasses import dataclass, field
 from typing import Optional
-from data import HMMDataset, MixtureOfHmms
+from data import HMMDataset, MixtureOfHmms, HMMInContextDataset
+from copy import deepcopy
+import numpy as np
+from collections import defaultdict
+
+import pandas as pd
+from plotnine import ggplot, aes, geom_point, facet_wrap, geom_line, stat_summary
 
 device = "cpu" if not torch.cuda.is_available() else "cuda"
 
 @dataclass
 class ModelArguments:
-    model_type: str = field(default="llama", metadata={"help": "Model architecture."})
-    num_hidden_layers: int = field(default=2, metadata={"help": "Number of layers in the transformer."})
+    model_type: str = field(default="gpt2", metadata={"help": "Model architecture."})
+    num_hidden_layers: int = field(default=4, metadata={"help": "Number of layers in the transformer."})
 
 
 @dataclass
 class DataArguments:
-    num_hmms: int = field(default=5, metadata={"help": "Number of HMMs in the mixture."})
-    num_hidden_states: int = field(default=5, metadata={"help": "Number of hidden states in each HMM."})
-    num_emissions: int = field(default=5, metadata={"help": "Number of emissions in each HMM."})
+    num_hmms: int = field(default=2, metadata={"help": "Number of HMMs in the mixture."})
+    num_entities: int = field(default=5, metadata={"help": "Number of entities in each HMM."})
+    num_properties: int = field(default=5, metadata={"help": "Number of properties in each HMM."})
+    num_emissions: int = field(default=100, metadata={"help": "Number of emissions in each HMM."})
     uniform_weights: Optional[bool] = field(default=True, metadata={"help": "Whether to use uniform weights for the mixture."})
-    num_train_examples: int = field(default=100, metadata={"help": "Number of training examples."})
-    num_eval_examples: int = field(default=10, metadata={"help": "Number of evaluation examples."})
-    xie: Optional[bool] = field(default=True, metadata={"help": "Whether to use Xie's HMM."})
+    num_train_examples: int = field(default=1000, metadata={"help": "Number of training examples."})
+    num_eval_examples: int = field(default=50, metadata={"help": "Number of evaluation examples."})
+    # xie: Optional[bool] = field(default=True, metadata={"help": "Whether to use Xie's HMM."})
+    k: int = field(default=3, metadata={"help": "Length of each in-context example."})
+    num_in_context_examples: int = field(default=200, metadata={"help": "Number of in-context examples."})
+    num_in_context_shots: int = field(default=64, metadata={"help": "Number of in-context shots."})
 
 
 @dataclass
@@ -31,10 +41,69 @@ class TrainingArguments(transformers.TrainingArguments):
     logging_strategy: str = field(default="steps", metadata={"help": "Log every n steps or every n epochs."})
     remove_unused_columns: bool = field(default=True)
     wandb_project: str = field(default="toy-alignment")
-    wandb_entity: str = field(default="none")
-    per_device_train_batch_size: int = field(default=1)
-    per_device_eval_batch_size: int = field(default=1)
-    gradient_accumulation_steps: int = field(default=1)
+    wandb_entity: str = field(default="aryamanarora")
+    per_device_train_batch_size: int = field(default=2)
+    per_device_eval_batch_size: int = field(default=2)
+    gradient_accumulation_steps: int = field(default=4)
+    torch_compile: bool = field(default=True)
+    num_train_epochs: int = field(default=5)
+    learning_rate: float = field(default=8e-4)
+    warmup_ratio: float = field(default=0.1)
+    
+
+
+def in_context_eval(trainer, in_context_dataset, data_args, title="in_context_probs"):
+    # in-context eval
+    subsets = in_context_dataset.make_subsets()
+    in_context_probs = []
+    for hmm, subset in subsets.items():
+        result = trainer.predict(subset)
+        probs = torch.tensor(result.predictions).softmax(-1) # shape: (bs, seq, num_emissions)
+        shots = 0
+        for i in range(data_args.k - 2, probs.shape[1], data_args.k + 1):
+            label = result.label_ids[:, i] # shape: (bs,)
+            # get the prob of the correct label for each example
+            prob = probs[torch.arange(probs.shape[0]), i, label]
+            argmax = probs[torch.arange(probs.shape[0]), i].argmax(-1)
+            for j in range(len(prob)):
+                in_context_probs.append({
+                    "k": shots,
+                    "prob": prob[j].item(),
+                    "acc": 1 if (argmax[j] == label[j]).item() else 0,
+                    "nll": -torch.log(prob[j]).item(),
+                    "hmm": str(hmm)
+                })
+            shots += 1
+    
+    # plot each
+    df = pd.DataFrame(in_context_probs)
+    plot = (
+        ggplot(df, aes(x="k", y="acc")) +
+        # geom_point(alpha=0.1, stroke=0) +
+        facet_wrap("~hmm") +
+        stat_summary(group="k", geom="line")
+    )
+    plot.save(f"plots/{title}.png")
+    plot = (
+        ggplot(df, aes(x="k", y="prob")) +
+        geom_point(alpha=0.1, stroke=0) +
+        facet_wrap("~hmm") +
+        stat_summary(group="k", geom="line")
+    )
+    plot.save(f"plots/{title}_p.png")
+    plot = (
+        ggplot(df, aes(x="k", y="nll")) +
+        geom_point(alpha=0.1, stroke=0) +
+        facet_wrap("~hmm") +
+        stat_summary(group="k", geom="line")
+    )
+    plot.save(f"plots/{title}_nll.png")
+    plot = (
+        ggplot(df, aes(x="k", y="acc")) +
+        # geom_point(alpha=0.1, stroke=0) +
+        stat_summary(group="k", geom="line")
+    )
+    plot.save(f"plots/{title}_all.png")
 
 
 def train():
@@ -47,22 +116,32 @@ def train():
 
     # set up model
     config = transformers.CONFIG_MAPPING[model_args.model_type](
-        vocab_size=data_args.num_emissions if not data_args.xie else data_args.num_hidden_states * data_args.num_emissions,
+        vocab_size=data_args.num_emissions,
         num_hidden_layers=model_args.num_hidden_layers,
         num_attention_heads=12,
         num_key_value_heads=12,
         hidden_size=768,
+        max_position_embeddings=1024,
     )
+    print(config)
     model = transformers.AutoModelForCausalLM.from_config(config)
     n_params = sum({p.data_ptr(): p.numel() for p in model.parameters()}.values())
     print(f"Training new model from scratch - Total size={n_params/(10**6):.2f}M params")
 
     # set up data
-    hmms = MixtureOfHmms(num_hmms=data_args.num_hmms, num_hidden_states=data_args.num_hidden_states,
-                         num_emissions=data_args.num_emissions, uniform_weights=data_args.uniform_weights,
-                         xie=data_args.xie)
+    hmms = MixtureOfHmms(
+        num_hmms=data_args.num_hmms, num_entities=data_args.num_entities, num_properties=data_args.num_properties,
+        num_emissions=data_args.num_emissions, uniform_weights=data_args.uniform_weights,
+    )
     train_dataset = HMMDataset(hmms=hmms, num_train_examples=data_args.num_train_examples, sample_length=10240)
     eval_dataset = HMMDataset(hmms=hmms, num_train_examples=data_args.num_eval_examples, sample_length=1024)
+    in_context_dataset = HMMInContextDataset(
+        hmms=hmms, num_train_examples=data_args.num_in_context_examples,
+        k=data_args.k, num_in_context_shots=data_args.num_in_context_shots
+    )
+
+    # print(torch.tensor(hmms.score(train_dataset[0]["input_ids"][:20])).softmax(-1))
+    # input()
 
     # set up trainer
     trainer = transformers.Trainer(
@@ -74,8 +153,37 @@ def train():
     trainer.train()
 
     # eval
-    res = trainer.evaluate()
-    print(res)
+    subsets = eval_dataset.make_subsets()
+    results = defaultdict(dict)
+    for hmm, subset in subsets.items():
+        res = trainer.evaluate(eval_dataset=subset, metric_key_prefix=f'eval_{hmm}')
+        results[hmm]["base"] = res[f'eval_{hmm}_loss']
+
+    # in-context eval
+    in_context_eval(trainer, in_context_dataset, data_args, title="in_context_probs")
+
+    # sft
+    # old_weights = deepcopy(hmms.weights)
+    hmms.weights = np.zeros(hmms.num_hmms)
+    hmms.weights[0] = 1
+    sft_dataset = HMMDataset(hmms=hmms, num_train_examples=data_args.num_train_examples // 10, sample_length=10240)
+
+    # train
+    trainer.train_dataset = sft_dataset
+    trainer.train()
+
+    # eval
+    subsets = eval_dataset.make_subsets()
+    for hmm, subset in subsets.items():
+        res = trainer.evaluate(eval_dataset=subset, metric_key_prefix=f'sft_eval_{hmm}')
+        results[hmm]["sft"] = res[f'sft_eval_{hmm}_loss']
+    
+    # print
+    for hmm in sorted(list(results.keys())):
+        print(f"{hmm}: {results[hmm]['base']:.4f} -> {results[hmm]['sft']:.4f}")
+
+    # in-context eval
+    in_context_eval(trainer, in_context_dataset, data_args, title="in_context_probs_sft")
 
 
 if __name__ == "__main__":
