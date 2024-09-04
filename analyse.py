7e14203716b821313af9098af2262c61e9a70904
aryamanarora <aryaman.arora2020@gmail.com>
2024-09-04 14:50:47 -0700

    fix logistic

--- analyse.py
+++ analyse.py
@@ -31,18 +31,34 @@ class PowerLawFit(torch.nn.Module):
         self.C = torch.nn.Parameter(torch.tensor(1.0))
         self.alpha = torch.nn.Parameter(torch.tensor(1.0))
         self.K = torch.nn.Parameter(torch.tensor(1.0))
+
+    def get_C(self):
+        return self.C.exp()
+    
+    def get_alpha(self):
+        return self.alpha.exp()
+    
+    def get_K(self):
+        return self.K.exp()
     
     def get_params(self):
         return {
-            "C": self.C.exp().item(),
-            "alpha": self.alpha.exp().item(),
-            "K": self.K.exp().item()
+            "C": self.get_C(),
+            "alpha": self.get_alpha(),
+            "K": self.get_K(),
         }
     
     def forward(self, shots):
+        C = self.get_C()
+        alpha = self.get_alpha()
+        K = self.get_K()
         if type(shots) == int:
             shots = torch.tensor([shots], dtype=torch.float32).to(DEVICE)
-        return self.C.exp() * (shots).pow(-self.alpha.exp()) + self.K.exp()
+        return C * (shots).pow(-alpha) + K
+
+    def estimate_nll(self, max_shots: int, hmm: int=None):
+        shots = torch.tensor(range(1, max_shots), dtype=torch.float32).to(DEVICE)
+        return self(shots)
 
 
 class BoundedPowerLawFit(torch.nn.Module):
@@ -52,19 +68,39 @@ class BoundedPowerLawFit(torch.nn.Module):
         self.alpha = torch.nn.Parameter(torch.tensor(1.0))
         self.K = torch.nn.Parameter(torch.tensor(1.0))
         self.n_c = torch.nn.Parameter(torch.tensor(1.0))
+    
+    def get_C(self):
+        return self.C.exp()
+
+    def get_alpha(self):
+        return self.alpha.exp()
+    
+    def get_K(self):
+        return self.K.exp()
+    
+    def get_n_c(self):
+        return self.n_c.exp()
 
     def get_params(self):
         return {
-            "C": self.C.exp().item(),
-            "alpha": self.alpha.exp().item(),
-            "K": self.K.exp().item(),
-            "n_c": self.n_c.exp().item()
+            "C": self.get_C().item(),
+            "alpha": self.get_alpha().item(),
+            "K": self.get_K().item(),
+            "n_c": self.get_n_c().item()
         }
     
     def forward(self, shots):
+        C = self.get_C()
+        alpha = self.get_alpha()
+        K = self.get_K()
+        n_c = self.get_n_c()
         if type(shots) == int:
             shots = torch.tensor([shots], dtype=torch.float32).to(DEVICE)
-        return self.C.exp() * (1 + shots / self.n_c.exp()).pow(-self.alpha.exp()) + self.K.exp()
+        return C * (1 + shots / n_c).pow(-alpha) + K
+
+    def estimate_nll(self, max_shots: int, hmm: int=None):
+        shots = torch.tensor(range(1, max_shots), dtype=torch.float32).to(DEVICE)
+        return self(shots)
 
 
 class LogisticLawFit(torch.nn.Module):
@@ -75,23 +111,45 @@ class LogisticLawFit(torch.nn.Module):
         self.K = torch.nn.Parameter(torch.tensor(1.0))
         self.n_c = torch.nn.Parameter(torch.tensor(1.0))
     
+    def get_C(self):
+        return torch.sigmoid(self.C)
+    
+    def get_alpha(self):
+        return self.alpha.exp()
+    
+    def get_K(self):
+        return torch.sigmoid(self.K)
+    
+    def get_n_c(self):
+        return self.n_c
+    
     def get_params(self):
         return {
-            "C": self.C.exp().item(),
-            "alpha": self.alpha.exp().item(),
-            "K": self.K.exp().item(),
-            "n_c": self.n_c.item()
+            "C": self.get_C().item(),
+            "alpha": self.get_alpha().item(),
+            "K": self.get_K().item(),
+            "n_c": self.get_n_c().item()
         }
     
     def forward(self, shots):
+        C = self.get_C()
+        alpha = self.get_alpha()
+        K = self.get_K()
+        n_c = self.get_n_c()
         if type(shots) == int:
             shots = torch.tensor([shots], dtype=torch.float32).to(DEVICE)
-        return -(self.C.exp() / (1 + (-self.alpha.exp() * (shots - self.n_c)).exp()) + self.K.exp()).log()
+        res = -((C - K) / (1 + (-alpha * (shots.log() - n_c)).exp()) + K).log()
+        return res
+
+    def estimate_nll(self, max_shots: int, hmm: int=None):
+        shots = torch.tensor(range(1, max_shots), dtype=torch.float32).to(DEVICE)
+        return self(shots)
 
 
 class BayesianLawFit(torch.nn.Module):
-    def __init__(self, num_hmms, sft_amounts=None):
+    def __init__(self, num_hmms, sft_amounts=None, do_log_shots=False):
         super(BayesianLawFit, self).__init__()
+        self.do_log_shots = do_log_shots
         self.num_hmms = num_hmms
         self.masks = torch.eye(num_hmms, dtype=torch.bool).to(DEVICE)
         if sft_amounts is not None:
@@ -130,6 +188,8 @@ class BayesianLawFit(torch.nn.Module):
     def forward(self, shots, hmm, sft_amount=[0], add_metrics=False):
         priors = self.get_prior(sft_amount).log()
         K = self.get_K(sft_amount)
+        if self.do_log_shots:
+            shots = shots.log()
         shots = shots * K
         if isinstance(shots, torch.Tensor):
             shots = shots.unsqueeze(-1)
@@ -150,15 +210,20 @@ class BayesianLawFit(torch.nn.Module):
         # input()
         if add_metrics:
             return {
-                "nll": nll,
+                "nll": est_nll,
                 "posteriors": posteriors,
             }
         return est_nll
 
+    def estimate_nll(self, max_shots: int, hmm: int=None):
+        shots = torch.tensor(range(1, max_shots), dtype=torch.float32).to(DEVICE)
+        hmm = torch.zeros_like(shots, dtype=torch.int32).fill_(hmm).to(DEVICE)
+        return self(shots, hmm)
+
 
 class BayesianLawSamplingFit(BayesianLawFit):
-    def __init__(self, num_hmms, sft_amounts=None):
-        super(BayesianLawSamplingFit, self).__init__(num_hmms, sft_amounts)
+    def __init__(self, num_hmms, sft_amounts=None, do_log_shots=False):
+        super(BayesianLawSamplingFit, self).__init__(num_hmms, sft_amounts, do_log_shots)
         self.gammas = torch.nn.Parameter(torch.zeros(num_hmms) + 1)
         self.betas = torch.nn.Parameter(torch.zeros(num_hmms) - 1)
     
@@ -184,8 +249,8 @@ class BayesianLawSamplingFit(BayesianLawFit):
 
 
 class BayesianLawScoringFit(BayesianLawFit):
-    def __init__(self, num_hmms, sft_amounts=None):
-        super(BayesianLawScoringFit, self).__init__(num_hmms, sft_amounts)
+    def __init__(self, num_hmms, sft_amounts=None, do_log_shots=False):
+        super(BayesianLawScoringFit, self).__init__(num_hmms, sft_amounts, do_log_shots)
         self.gammas = torch.nn.Parameter(torch.zeros(num_hmms) + 1)
         self.betas = torch.nn.Parameter(torch.zeros(num_hmms) - 1)
 
@@ -239,12 +304,13 @@ def fit_bayesian_law(
     epochs: int=50,
     lr: float=5e-2,
     num_hmms: int=None,
+    do_log_shots: bool=False,
 ):
     # fit power law
     sft_amounts = None if not sft_amount else list(subset['sft_amount'].unique())
     law_type = bayesian_law_mapping[type]
     num_hmms = num_hmms or len(subset['hmm'].unique())
-    model = law_type(num_hmms, sft_amounts)
+    model = law_type(num_hmms, sft_amounts, do_log_shots)
     model.to(DEVICE)
     iterator = tqdm(range(epochs)) if not quiet else range(epochs)
     history = []
@@ -266,7 +332,7 @@ def fit_bayesian_law(
                 est_nll = model(shots, hmm, sft_amounts)
             else:
                 est_nll = model(shots, hmm)
-            loss = ((true_nll - est_nll)**2).sum()
+            loss = ((true_nll.log() - est_nll.log())**2).sum()
             loss.backward()
             optimizer.step()
             avg_loss += loss.item()
@@ -314,7 +380,7 @@ def fit_power_law(
             shots = torch.tensor(batch['shots'].values, dtype=torch.float32).to(DEVICE)
             true_nll = torch.tensor(batch['nll'].values, dtype=torch.float32).to(DEVICE)
             est_nll = model(shots)
-            loss = ((true_nll - est_nll)**2).sum()
+            loss = ((true_nll.log() - est_nll.log())**2).sum()
             loss.backward()
             optimizer.step()
             avg_loss += loss.item()
